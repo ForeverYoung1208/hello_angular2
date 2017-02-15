e2a8343a2b0016e33e3fc699977babf406e1b222
Igor <siafin2010@gmail.com>
2017-02-15 18:03:28 +0200

    a

--- app/app.module.js
+++ app/app.module.js
@@ -10,16 +10,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
+var forms_1 = require('@angular/forms');
 var app_component_1 = require('./app.component');
 var hello2_component_1 = require('./hello2/hello2.component');
 var todoblock_component_1 = require('./todoblock/todoblock.component');
-// import { TodoItem }  from './todoitem/todoitem.component';
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
+            imports: [platform_browser_1.BrowserModule,
+                forms_1.FormsModule
+            ],
             declarations: [app_component_1.AppComponent,
                 hello2_component_1.Hello2Component,
                 todoblock_component_1.TodoBlockComponent
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { Hello2Component }  from './hello2/hello2.component';
 import { TodoBlockComponent }  from './todoblock/todoblock.component';
-// import { TodoItem }  from './todoitem/todoitem.component';
-
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule,
+								  FormsModule
+								],
   declarations: [ AppComponent, 
   								Hello2Component, 
-  								TodoBlockComponent 
+  								TodoBlockComponent
   							],
   bootstrap:    [ AppComponent ]
 })
